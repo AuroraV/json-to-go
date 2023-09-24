dcd1f8f23383d252c5fc6da87e549c37353abd0f
AuroraV <zzl_auroras@163.com>
2023-09-24 23:38:47 +0800

    feat(add):j2g support gofmt output

--- cli/cli.go
+++ cli/cli.go
@@ -3,9 +3,16 @@ package cli
 import (
 	"fmt"
 	j2g "github.com/AuroraV/json-to-go"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
 )
 
 const (
+	tmpFilePrefix     = "tmp-file-g2j-"
+	goFilePackageHead = "package main"
+
 	exitCodeOK = iota
 	exitCodeFalsyErr
 	exitCodeFlagParseErr
@@ -17,6 +24,7 @@ const (
 type cli struct{}
 
 type flagOpts struct {
+	Format  bool   `short:"f" long:"format" description:"output format(by gofmt)"`
 	Inline  bool   `short:"i" long:"inline" description:"output inline"`
 	Stream  string `short:"s" long:"stream" description:"input stream"`
 	Version bool   `short:"v" long:"version" description:"display version information"`
@@ -39,8 +47,58 @@ func (cli *cli) runInternal(args []string) (err error) {
 	}
 	meta := j2g.Parse(opts.Stream)
 	result := j2g.GenerateStruct(meta, opts.Inline)
+
+	if opts.Format {
+		return gofmtOutput(result)
+	}
+	return normalOutput(result)
+}
+
+func genFile() string {
+	return fmt.Sprintf("%s%d", tmpFilePrefix, time.Now().UnixNano())
+}
+
+func normalOutput(result []string) error {
 	for _, item := range result {
 		fmt.Println(item)
 	}
+	return nil
+}
+
+func gofmtOutput(result []string) error {
+	f, err := os.CreateTemp("", genFile())
+	if err != nil {
+		return err
+	}
+	defer func() {
+		_ = f.Close()
+		_ = os.Remove(f.Name())
+	}()
+	if len(result) > 0 {
+		_, err := f.WriteString(goFilePackageHead + "\n")
+		if err != nil {
+			return err
+		}
+	}
+	for _, item := range result {
+		_, err := f.WriteString(item + "\n")
+		if err != nil {
+			return err
+		}
+	}
+	fmt.Println(f.Name())
+	cmd := exec.Command("gofmt", "-w", f.Name())
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(fmt.Sprintf("gofmt output: %s", string(out)))
+		return err
+	}
+	body, err := os.ReadFile(f.Name())
+	if err != nil {
+		return err
+	}
+	content, _ := strings.CutPrefix(string(body), goFilePackageHead)
+	content, _ = strings.CutPrefix(content, "\n")
+	fmt.Println(content)
 	return nil
 }
